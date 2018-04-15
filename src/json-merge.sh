a69a9a8d6352dcbdb0b4a3b8410bbeaaa84d131e
Marcus Manley <3m@marcuss-mbp.lan>
2018-04-15 14:58:35 -0500

    JSON Updates

--- src/json-merge.sh
+++ src/json-merge.sh
@@ -1,2 +1,9 @@
-jq -s '[ .[0] + .[1] | group_by(.Name)[] | select(length > 1) | add ]' data.json MNData1.json
+jq -s '[ .[0] + .[1] | group_by(.properties.Name)[] | select(length > 1) | add ]' data.json MNDist.json
 
+jq -s '[.[0] * .[1] | add ]' data1.json MNData2.json 
+
+cat data.json | jq '.Name'
+
+cat data.json | jq '.[] | .Name'
+
+cat MNDist.json | jq '.[] | .properties.Name'
